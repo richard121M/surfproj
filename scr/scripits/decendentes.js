e3507158056a1ae0d09b46a1c2de9d9033c977cc
richard121M <richardtavares520@gmail.com>
2026-09-05 09:45:52 -0300

    surfv0.015

--- scr/scripits/decendentes.js
+++ scr/scripits/decendentes.js
@@ -1,4 +1,5 @@
 import {conectedDB, GETinfo, addHTML} from './geral.js'
+import {CriarGrade} from './logic/decendentsLogic.js'
 
 function CreateBrick(coluna,BrickMateria,BolhaMateria,flag=''){
     let colunaElement = document.getElementById(`co${coluna}`);
@@ -39,7 +40,7 @@ function CreateBrick(coluna,BrickMateria,BolhaMateria,flag=''){
 
 }
 
-export async function CriarGrade(codMat = []) {
+export async function CriarGradee(codMat = []) {
     const MATERIAS = await conectedDB();
     let codigo = document.getElementById("codigo").value.trim();
     if (codMat.length > 0){
@@ -156,8 +157,137 @@ export async function CriarGrade(codMat = []) {
     document.getElementById("codigo").focus();
 }
 
+function CreateBrick(coluna,BrickMateria,BolhaMateria,flag=''){
+    let colunaElement = document.getElementById(`co${coluna}`);
+    if (colunaElement == null){
+        alert('coluna nao encotrada')
+        return 'C'
+    }
+    let BMcodigo = '';
+    let BMnome = '';
+    BrickMateria.map((BM) =>{
+        BMcodigo += BM.codigo;
+        BMnome += " - "+BM.nome+ " -";
+    })
+    let brickElement = document.getElementById(`B-${BMcodigo}`);
+    if (brickElement == null ){
+        colunaElement.innerHTML += `<div class="bricks" id="B-${BMcodigo}"> - ${BMnome} - </div>`
+    }
+    brickElement = document.getElementById(`B-${BMcodigo}`);
+    if (flag != 'nope'){
+        brickElement.innerHTML += `<div class="bolha${flag}">
+        <div class='CH'>
+            <p>${BolhaMateria.cargaHoraria}</p></div>
+            <div><p>${BolhaMateria.codigo} - ${BolhaMateria.nome} </p></div>
+        </div>`;
+
+    }else{
+        brickElement.innerHTML += `<div class="bolha${flag}">
+            <div class="divnope">
+                <div class='CH'><p>${BolhaMateria.cargaHoraria}</p></div>
+                <div><p>${BolhaMateria.codigo} - ${BolhaMateria.nome} </p></div>
+            </div>
+            <div>
+                <p>${BolhaMateria.prerequisito}</p>
+            </div>
+        </div>`;
+        
+    }
+
+}
+
+export async function GerarGrade(codMat = []) {
+    const MATERIAS = await conectedDB();
+    let codigo = document.getElementById("codigo").value.trim();
+    if (codMat.length > 0){
+        codigo = codMat[0]
+    }
+    //let check = document.getElementById("preRequesi");
+    let listGrade = [[GETinfo(codigo,MATERIAS)],];
+    let listamate2 = [{mat : GETinfo(codigo,MATERIAS), coluna: 0}];
+
+    let grade = document.getElementById("quadro");
+    grade.innerHTML = '';
+    let id = 1;
+  
+        ///METERIAS AZUIS///
+        ///===========================================================//
+        for (let i = 0; i<6; i+= 1){
+            grade.innerHTML += `<div class="colMate" id=co${i+1}></div>`;
+
+            let adicionou = false;
+            let lista_ = listGrade[id-1]; 
+            lista_.map((mate) => {
+                    for (let materia of MATERIAS){
+                        if (materia.prerequisito.length > 0){
+                        let prerequisitos = materia.prerequisito
+                            if (prerequisitos.includes(mate.codigo)){
+
+                                if (adicionou == false){
+                                    listGrade.push([ ]);
+                                }
+
+                                CreateBrick(i+1,[mate],materia)
+                                adicionou = true;
+                                listamate2.push({mat : materia, coluna: i+1})
+                                listGrade[id].push(materia);   
+                            }
+                        }
+                    }
+                })
+            if (adicionou == false){
+                break;
+            }
+            id += 1;
+        }
+        ///METERIAS AMARELAS///
+
+        for (let materia of MATERIAS){
+            let prerequisitos = materia.prerequisito
+            if (prerequisitos.length > 0){
+                prerequisitos.map((pre) => {
+                    if (pre.includes("&")){
+                        let PRE = pre.split("&")
+                        let cont = PRE.length;
+                        let col = 0;
+                    
+                        let ListBM = [];
+                        listamate2.map((mater) =>{
+                            if (PRE.includes(mater.mat.codigo)){
+                                cont -= 1;
+
+                                col = mater.coluna +1;
+                                ListBM.push(mater.mat)
+                                if (cont == 0){
+                                    CreateBrick(col,ListBM,materia,'nop')    
+                                }
+                            }
+                        })
+                        if (cont == 0){
+                            listamate2.push({mat : materia, coluna: col})
+                            listGrade[col-1].push(materia);   
+                        }
+                    }
+                })
+            }
+        }
+
+        ///METERIAS VERMELHAS///
+        
+        let ultimaColuna = grade.lastElementChild;
+        if (ultimaColuna.lastElementChild == null){
+            ultimaColuna.remove()
+        }
+    
+
+    document.getElementById("codigo").value = "";
+    document.getElementById("nome").value = "";
+    document.getElementById("codigo").focus();
+}
+
+
 async function PesquiNome(codigo){
-    await CriarGrade([codigo])
+    await GerarGrade([codigo])
 }
 
 
---
+++ scr/scripits/logic/decendentsLogic.js
@@ -0,0 +1,86 @@
+import {conectedDB, GETinfo, addHTML} from '../geral.js'
+
+// {} = {
+//     "codigo" : nausddniasd,
+//     "nomeBolha" : calculo I,
+
+//     "listMaterias" : [
+//         {"type": "nop",
+//          "codigo" :"ashbdfabsh",
+//          "prerequisito" :"ashbdfabsh"
+//         }
+//     ]
+
+// }
+
+// [
+//     [{},{},{}],
+//     [{},{},{}],
+//     [{},{},{}],
+//     [{},{},{}],
+    
+// ]
+function listMateAntecessoras(listGrade, mateMiseri){
+    let newListGrade = []
+    if (listGrade == null){
+        return [mateMiseri.codigo];
+    }
+    alert(12)
+
+    if (listGrade.length > 0){
+        newListGrade = listGrade
+    }
+    
+    let listMate = []
+    alert(newListGrade)
+    newListGrade.map((mate) => {
+        for (let mate2 of mate.listMaterias) {
+            alert(13);
+            if (!listMate.includes(mate2.codigo)) {listMate.push(mate2.codigo)}
+        }
+    })
+    return listMate;
+}
+
+
+export async function CriarGrade(codMat = []) {
+    const MATERIAS = await conectedDB();
+    let codigo = ""
+    if (codMat.length > 0){codigo = codMat[0]}
+    let materiaPesquisada = GETinfo(codigo,MATERIAS);
+    let gradeMaterias = []
+    
+    ///METERIAS AZUIS///
+    ///===========================================================//
+    let id = 0;
+    for (let i = 0; i<6; i+= 1){
+        let coluna = [];
+        let materiasAntecessoras = listMateAntecessoras(gradeMaterias[id-1],materiaPesquisada);
+        alert(materiasAntecessoras)
+        materiasAntecessoras.map((mate) => {
+            coluna.push({ "codigo" : mate, "nomeBolha" : GETinfo(mate,MATERIAS).nome,"listMaterias" : [] })
+            for (let materia of MATERIAS){
+                if (materia.prerequisito.length > 0){
+                    if (materia.prerequisito.includes(mate)){
+                        coluna.at(-1).listMaterias.push({ "type" : "direto", "codigo" : materia.codigo, "prerequisito" : materia.prerequisito })
+                    }
+                }
+            }
+        })
+        alert(coluna)
+        if (gradeMaterias.length == 0){
+            gradeMaterias[id] = coluna
+        }else{
+            gradeMaterias.push(coluna)
+        }
+        if (coluna.length == 0){
+            break;
+        }
+        id += 1;
+    }
+    alert(gradeMaterias[3].at(0).nomeBolha)
+    return gradeMaterias;
+    ///===========================================================//
+}
+
+// ========================================================================  //
